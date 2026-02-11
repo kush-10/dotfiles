05605e9ee09c7da17f213819a9b13d4a60954807
Kushal <kush.al@outlook.com>
2026-02-11 10:39:57 +0000

    update remap

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
