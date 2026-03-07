4e0bfa720052e7e3dcf2ddff9fa893b6090f8b0b
Kushal <kush.al@outlook.com>
2026-03-07 14:46:43 +0000

    set.lua

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -14,6 +14,7 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.wo.wrap = false
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
